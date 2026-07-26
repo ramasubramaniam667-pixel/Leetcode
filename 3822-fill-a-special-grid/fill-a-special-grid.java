eb02ed416a71f57bb819359c04c30b79aa3f0f7b
ramasubramaniam667-pixel <ramasubramaniam667@gmail.com>
2026-07-26 08:25:56 +0530

    Time: 23 ms (5.56%) | Memory: 109 MB (88.89%) - LeetSync

---
+++ 3822-fill-a-special-grid/fill-a-special-grid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] specialGrid(int n) {
+          int size = 1 << n;
+          int [][] grid = new int[size][size];
+          for (int i = 0; i < n ; i++){
+              for (int r = 0; r < size; r++){
+                  for(int c = 0;c < size;c++){
+                    int temp =  (r >> i) & 1;
+                    int y = (c >> i) & 1 ;
+                    if (temp == 0) grid[r][c] += ((y == 0 ? 3:0) << (2*i));
+                    else grid[r][c] += ((y == 0 ? 2:1) << (2*i));
+                  }
+              }
+          }return grid;
+    } 
+}
